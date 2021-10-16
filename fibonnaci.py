5c18551ba53950eb7b0a5e1ae501dec33a30920c
Mr.Incredible.29 <65172813+Naitik-Jain@users.noreply.github.com>
2021-10-16 23:04:32 +0530

    Update fibonnaci.py

--- fibonnaci.py
+++ fibonnaci.py
@@ -1,17 +1,17 @@
 
 import math 
 def isPerfectSquare(x): 
-	s = int(math.sqrt(x)) 
-	return s*s == x 
+s = int(math.sqrt(x)) 
+return s*s == x 
 
 
 def isFibonacci(n): 
 
-	return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4) 
+return isPerfectSquare(5*n*n + 4) or isPerfectSquare(5*n*n - 4) 
 	
 
 for i in range(1,11): 
-	if (isFibonacci(i) == True): 
+if (isFibonacci(i) == True): 
 		print i,"is a Fibonacci Number"
-	else: 
+else: 
 		print i,"is a not Fibonacci Number "
